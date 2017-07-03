5c1e4b101e20645acb2131ad9ea2f40c6a5ee517
Iggi <iggirex@gmail.com>
2017-07-03 17:27:44 -0600

    Added backend and Signup

--- tutsPlus/reactBlog/html/main.jsx
+++ tutsPlus/reactBlog/html/main.jsx
@@ -10,7 +10,16 @@ class Signin extends React.Component {
     };
   }
   signIn(){
-    alert('Email is ' + this.state.email + ' Password is ' + this.state.password);
+    axios.post('/signin', {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then(function(response){
+      console.log(response);
+    })
+    .catch(function(error){
+      console.log(error)
+    });
   }
   handleEmailChange(e){
     this.setState({email:e.target.value})
@@ -33,6 +42,24 @@ class Signin extends React.Component {
     )
   }
 }
+
+class SignUp extends React.Component{
+  render(){
+    return(
+      <div>
+        <form className="form-signin">
+          <h2 className="form-signin-heading">Please Sign Up</h2>
+          <label for="inputName" className="sr-only">Name</label>
+          <input type="name" onChange={this.handleNameChange} id="inputName" className="form-control" placeholder="Name" required autofocus />
+          <label for="inputPassword" className="sr-only">Password</label>
+          <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required />
+          <button className="btn btn-lg btn-primary btn-block" onClick={this.signUp} type="button">Sign Up</button>
+        </form>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
   <Signin />,
   document.getElementById("app")
